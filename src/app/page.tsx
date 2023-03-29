27aa78b3cf1415f6a61b23ce8ab9df5941378bad
mrolaleyeistrael <mrolaleyeistrael@gmail.com>
2023-03-29 13:32:09 -0700

    Experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (<>
     <div className=' md:w-[80%] md:mx-auto mt-20 '>
       <Intro />
-      <div className=" grid grid-cols-1 md:grid-cols-[1fr_2fr] gap-10 ">
+      <div className=" h-fit grid grid-cols-1 md:grid-cols-[1fr_2fr] gap-10 ">
         <Skills />
         <Experience />
       </div>
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -5,66 +5,54 @@ import Image from 'next/image'
 const Experience = () => {
     return (
         <Card>
-            <h1>Experiences</h1>
+            <h1 className=' font-bold '>Experiences</h1>
 
             <div className=' mt-11 space-y-8 '>
+
                 <div className=' flex flex-row '>
-                    <div className=' w-[100px] mr-4 md:mr-6 md:w-[150px] shrink-0 pt-4 '>
-                        <Image src={'/images/gathpay-logo-blue.svg'} alt="" width={200} height={200} className='' />
+                    <div className=' w-[100px] mr-4 md:mr-6 md:w-[100px] shrink-0 pt-4 '>
+                        <Image src={'/images/kodeCamp.jpg'} alt="" width={200} height={200} className='' />
                     </div>
                     <div>
-                        <p>Dec 2022 - Current</p>
-                        <h1 className=' mt-3 font-bold text-[#333333] '>Front-end Developer</h1>
-                        <p className=' mt-8 text-[#828282] '>Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.</p>
+                        <p>Mar 2022 - July 2022</p>
+                        <h1 className=' mt-3 font-bold text-[#333333] '>Front-end Developer Intern</h1>
+                        <p className=' mt-4 text-[#828282] '>Created a scalable progressive web app with ReactJS that allowed
+                            cross-platform accessibility to the website on any device with primary use on
+                            mobile.</p>
                     </div>
                 </div>
 
                 <div className=' flex flex-row '>
-                    <div className=' w-[100px] mr-4 md:mr-6 md:w-[150px] shrink-0 pt-4 '>
-                        <Image src={'/images/gathpay-logo-blue.svg'} alt="" width={200} height={200} className='' />
+                    <div className=' w-[100px] mr-4 md:mr-6 shrink-0 pt-4 '>
+                        <Image src={'/images/Zuri-Team-Logo.svg'} alt="" width={200} height={200} className='' />
                     </div>
                     <div>
-                        <p>Dec 2022 - Current</p>
-                        <h1 className=' mt-3 font-bold text-[#333333] '>Front-end Developer</h1>
-                        <p className=' mt-8 text-[#828282] '>Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.</p>
+                        <p>May 2022 - Aug 2022</p>
+                        <h1 className=' mt-3 font-bold text-[#333333] '>Front-end Developer Intern</h1>
+                        <p className=' mt-4 text-[#828282] '>I worked hand-in-hand with the Backend and the UI team to build a platform that
+                            splits, CSV and JSON files into chunks. I also worked on several responsive
+                            and pixel-perfect sites (using ReactJS, Styled Components, TailwindCSS) which
+                            were templated by other backend interns. I learnt a lot during the process.</p>
                     </div>
                 </div>
 
                 <div className=' flex flex-row '>
-                    <div className=' w-[100px] mr-4 md:mr-6 md:w-[150px] shrink-0 pt-4 '>
+                    <div className=' w-[100px] mr-4 md:mr-6  shrink-0 pt-4 '>
                         <Image src={'/images/gathpay-logo-blue.svg'} alt="" width={200} height={200} className='' />
                     </div>
                     <div>
                         <p>Dec 2022 - Current</p>
                         <h1 className=' mt-3 font-bold text-[#333333] '>Front-end Developer</h1>
-                        <p className=' mt-8 text-[#828282] '>Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.</p>
+                        <p className=' mt-4 text-[#828282] '>Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.</p>
                     </div>
                 </div>
 
-                <div className=' flex flex-row '>
-                    <div className=' w-[100px] mr-4 md:mr-6 md:w-[150px] shrink-0 pt-4 '>
-                        <Image src={'/images/gathpay-logo-blue.svg'} alt="" width={200} height={200} className='' />
-                    </div>
-                    <div>
-                        <p>Dec 2022 - Current</p>
-                        <h1 className=' mt-3 font-bold text-[#333333] '>Front-end Developer</h1>
-                        <p className=' mt-8 text-[#828282] '>Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.</p>
-                    </div>
-                </div>
 
-                <div className=' flex flex-row '>
-                    <div className=' w-[100px] mr-4 md:mr-6 md:w-[150px] shrink-0 pt-4 '>
-                        <Image src={'/images/gathpay-logo-blue.svg'} alt="" width={200} height={200} className='' />
-                    </div>
-                    <div>
-                        <p>Dec 2022 - Current</p>
-                        <h1 className=' mt-3 font-bold text-[#333333] '>Front-end Developer</h1>
-                        <p className=' mt-8 text-[#828282] '>Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.</p>
-                    </div>
-                </div>
+
+
             </div>
 
-            
+
         </Card>
     )
 }

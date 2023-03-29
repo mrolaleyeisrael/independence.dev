ff5b8cd37fc648826fdb773f328970f3e2aabdc9
mrolaleyeistrael <mrolaleyeistrael@gmail.com>
2023-03-29 07:32:02 -0700

    Experience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,10 +1,70 @@
 import React from 'react'
 import Card from './Card/Card'
+import Image from 'next/image'
 
 const Experience = () => {
     return (
         <Card>
-            Expereince
+            <h1>Experiences</h1>
+
+            <div className=' mt-11 space-y-8 '>
+                <div className=' flex flex-row '>
+                    <div className=' w-[100px] mr-4 md:mr-6 md:w-[150px] shrink-0 pt-4 '>
+                        <Image src={'/images/gathpay-logo-blue.svg'} alt="" width={200} height={200} className='' />
+                    </div>
+                    <div>
+                        <p>Dec 2022 - Current</p>
+                        <h1 className=' mt-3 font-bold text-[#333333] '>Front-end Developer</h1>
+                        <p className=' mt-8 text-[#828282] '>Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.</p>
+                    </div>
+                </div>
+
+                <div className=' flex flex-row '>
+                    <div className=' w-[100px] mr-4 md:mr-6 md:w-[150px] shrink-0 pt-4 '>
+                        <Image src={'/images/gathpay-logo-blue.svg'} alt="" width={200} height={200} className='' />
+                    </div>
+                    <div>
+                        <p>Dec 2022 - Current</p>
+                        <h1 className=' mt-3 font-bold text-[#333333] '>Front-end Developer</h1>
+                        <p className=' mt-8 text-[#828282] '>Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.</p>
+                    </div>
+                </div>
+
+                <div className=' flex flex-row '>
+                    <div className=' w-[100px] mr-4 md:mr-6 md:w-[150px] shrink-0 pt-4 '>
+                        <Image src={'/images/gathpay-logo-blue.svg'} alt="" width={200} height={200} className='' />
+                    </div>
+                    <div>
+                        <p>Dec 2022 - Current</p>
+                        <h1 className=' mt-3 font-bold text-[#333333] '>Front-end Developer</h1>
+                        <p className=' mt-8 text-[#828282] '>Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.</p>
+                    </div>
+                </div>
+
+                <div className=' flex flex-row '>
+                    <div className=' w-[100px] mr-4 md:mr-6 md:w-[150px] shrink-0 pt-4 '>
+                        <Image src={'/images/gathpay-logo-blue.svg'} alt="" width={200} height={200} className='' />
+                    </div>
+                    <div>
+                        <p>Dec 2022 - Current</p>
+                        <h1 className=' mt-3 font-bold text-[#333333] '>Front-end Developer</h1>
+                        <p className=' mt-8 text-[#828282] '>Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.</p>
+                    </div>
+                </div>
+
+                <div className=' flex flex-row '>
+                    <div className=' w-[100px] mr-4 md:mr-6 md:w-[150px] shrink-0 pt-4 '>
+                        <Image src={'/images/gathpay-logo-blue.svg'} alt="" width={200} height={200} className='' />
+                    </div>
+                    <div>
+                        <p>Dec 2022 - Current</p>
+                        <h1 className=' mt-3 font-bold text-[#333333] '>Front-end Developer</h1>
+                        <p className=' mt-8 text-[#828282] '>Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.</p>
+                    </div>
+                </div>
+            </div>
+
+            
         </Card>
     )
 }
--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -13,7 +13,7 @@ const Intro = () => {
                     <Image src={'/images/me.jpg'} alt='' className="rounded-[12px] w-full shrink-0" width={271} height={200} />
                 </div>
                 <div className=' mt-6 md:mt-0 md:px-[33px] '>
-                    <div className=' grow flex flex-col  md:flex-row mb-[34px] md:justify-between '>
+                    <div className=' space-y-8 grow flex flex-col  md:flex-row mb-[34px] md:justify-between '>
                         <div>
                             <h1 className=' text-[#4F4F4F] text-2xl font-bold mb-2 '>Israel Olaleye</h1>
                             <p className=' text-lg text-[#828282] '>Front-end developer</p>
